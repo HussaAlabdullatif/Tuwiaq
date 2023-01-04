5f755c4e6fc0ec893d7dbd7d1a6b1da64f37a914
HussaAlabdullatif <121847555+HussaAlabdullatif@users.noreply.github.com>
2023-01-04 15:54:52 +0300

    Add files via upload

---
+++ ccc.kt
@@ -0,0 +1,31 @@
+fun main() {
+
+//Function مافهمت السؤال
+
+    nn(num1 = 10.0, num2 = 2.2, bb = "/")
+}
+
+
+
+fun nn (num1: Double  , num2: Double  , bb: String = "+" ) {
+    println(num1 / num2)
+
+
+// Array
+
+var NumAvr = arrayListOf<Double>()
+
+    NumAvr.add(5.5)
+    NumAvr.add(3.3)
+    NumAvr.add(2.2)
+    NumAvr.add(1.1)
+    NumAvr.add(4.4)
+    val sum = NumAvr[0]+NumAvr[1]+NumAvr[2]+NumAvr[3]+NumAvr[4]
+    println(sum)
+
+    for (i in NumAvr)
+        println(sum/5)
+
+}
+
+
